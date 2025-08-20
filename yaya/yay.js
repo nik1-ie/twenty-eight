606e91f80a20cef839d5286aa97f1a4bc5fe2467
niekita j. <niekitaj0805@gmail.com>
2025-08-20 21:38:41 +0200

    Create yay.js

---
+++ yaya/yay.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("i was thirteen when i dated this person online. it took me an entire month for my kid brain to realize how wrong it felt. since then, i was always dead set to not fall for anyone online, never, never.")
+console.log("maybe you guessed where this is going.")
+console.log("so yes, you somehow broke through the rules i set for myself and i am left with feelings that are most likely unrequited. but how could i guess what you feel about me; all i have are twisted assumptions i can make off of goofy texts that were never ever serious.")
+console.log("i don’t know how i fell, i don’t know why and i curse myself every day for ever falling for this. do i even know you enough to claim i love you ? do you know me enough to say anything back ?")
+console.log("i’m sorry for this half-assed confession letter, i’m sorry for maybe making this hard for you.")
+console.log("i know how to being friends and let the feelings pass. i can do that. but you know, maybe, just maybe ;")
